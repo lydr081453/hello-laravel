e3cb77b401a1a4f3f5e7aaec0ae6666f60870a20
vagrant <vagrant@homestead>
2018-12-24 10:02:53 +0000

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+    //微博模型的 fillable 属性中允许更新微博的 content 字段即可
+    protected $fillable = ['content'];
+
     //微博模型中，指明一条微博属于一个用户
     public function user()
     {
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,5 @@ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
